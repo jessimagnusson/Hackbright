8463d50c3443f9c403b613c36ad3567478457292
Your Name <you@example.com>
2016-07-21 21:18:19 -0700

    this is stupid

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+# name1 = "Zoz"
+# name2 = "Zoy"
+# name3 = "Noelis"
+
+# if name1 > name2:
+# 	print "My name is greater."
+# elif name1 < name2:
+# 	print "Their name is greater."
+# else:
+# 	print "Our names are equal."
+
+# print ord("Z"), ord("o"), ord("e")
+# print ord("z"), ord("o"), ord("y")
+
+
+date = 21
+if date > 15:
+	print "Oh, we're halfway there!"
+else: 
+	print "The month is still young"
+	 
